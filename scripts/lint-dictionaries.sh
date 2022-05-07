b597d0e926e007c1b7b4b902b83f1b0407874f39
Ben Cunningham <benjamescunningham@gmail.com>
2022-05-06 22:30:36 -0400

    Try it inline?

--- scripts/lint-dictionaries.sh
+++ scripts/lint-dictionaries.sh
@@ -2,7 +2,6 @@
 
 set -eo pipefail
 
-LC_ALL=
 GREEN="\033[0;32m"
 RED="\033[0;31m"
 NC="\033[0;0m"
@@ -14,7 +13,7 @@ for i in "${!dicts[@]}"; do
 
     iter="$((i + 1))/${#dicts[@]}"
     dict="${dicts[$i]}"
-    diff_with_sorted=$(sort -u "${dict}" | diff - "${dict}" || true)
+    diff_with_sorted=$(LC_ALL="en_US.UTF-8" sort -u "${dict}" | diff - "${dict}" || true)
 
     if [ -z "${diff_with_sorted}" ]; then
         echo -e "${iter} ${dict} ${GREEN}OK${NC}"
