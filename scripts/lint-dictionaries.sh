937361105bd46640b9e7fdbe8fd2388272167e80
Ben Cunningham <benjamescunningham@gmail.com>
2022-05-06 22:26:24 -0400

    Try... unsetting LC_ALL?

--- scripts/lint-dictionaries.sh
+++ scripts/lint-dictionaries.sh
@@ -2,6 +2,7 @@
 
 set -eo pipefail
 
+LC_ALL=
 GREEN="\033[0;32m"
 RED="\033[0;31m"
 NC="\033[0;0m"
@@ -13,7 +14,7 @@ for i in "${!dicts[@]}"; do
 
     iter="$((i + 1))/${#dicts[@]}"
     dict="${dicts[$i]}"
-    diff_with_sorted=$(sort -u "${dict}" | diff -y - "${dict}" || true)
+    diff_with_sorted=$(sort -u "${dict}" | diff - "${dict}" || true)
 
     if [ -z "${diff_with_sorted}" ]; then
         echo -e "${iter} ${dict} ${GREEN}OK${NC}"
